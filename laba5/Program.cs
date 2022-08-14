76b8d9276bbdf8dfd040b9d1a3de04d78dd19e14
pasxin <110281240+pasxin@users.noreply.github.com>
2022-08-14 23:10:04 +0300

    ДЗ № 2 Gb.ru

---
+++ laba5/Program.cs
@@ -0,0 +1,12 @@
+﻿// Программа, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+Console.WriteLine ("Введите 3-х значное число");
+string String = Console.ReadLine(); 
+int number = Convert.ToInt32(String); 
+number=Math.Abs(number);
+if (number>999 || number < 100) 
+    {Console.Write ("Введено не 3-х значное число");} 
+        else
+        {
+        int result = (number/10) % 10; 
+        Console.WriteLine (result);
+        }
