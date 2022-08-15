530725579df3113f17e82023902c10ec7eccac10
pasxin <110281240+pasxin@users.noreply.github.com>
2022-08-15 19:02:02 +0300

    игнор

---
+++ laba10/Program.cs
@@ -0,0 +1,14 @@
+﻿//Программа, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// Функция рассчета дельты в квадрате
+int cub (int x)
+{
+int result = x*x*x;
+return result;
+}
+//-------------Тело программы-------------
+Console.WriteLine ("Введиет значение N");
+string String = Console.ReadLine(); 
+int N = Convert.ToInt32(String); 
+for (int i=1; i <= N;i++)
+Console.WriteLine ($"{i} ^ 3 = {cub(i)}");
