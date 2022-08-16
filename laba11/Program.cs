d5a69c193e23e8882b6e15915be8d70aa7d72e36
pasxin <110281240+pasxin@users.noreply.github.com>
2022-08-16 18:38:18 +0300

    ДЗ № 4

---
+++ laba11/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) 
+//и возводит число A в натуральную степень B.
+// Math.Pow(A, B) - готово)
+Console.WriteLine ("Введиет значение A");
+string String = Console.ReadLine(); 
+int a = Convert.ToInt32(String); 
+Console.WriteLine ("Введиет значение B");
+String = Console.ReadLine(); 
+int b = Convert.ToInt32(String);
+int result = 1; 
+for (int count = 0; count < b; count ++)
+    result = result * a;
+Console.WriteLine($"А в степени В равна: {result}");
