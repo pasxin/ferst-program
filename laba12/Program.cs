f9ace6a37763ffd0f000aebd42b058ed3fc0a09a
pasxin <110281240+pasxin@users.noreply.github.com>
2022-08-16 18:49:44 +0300

    1

--- laba12/Program.cs
+++ laba12/Program.cs
@@ -5,7 +5,7 @@ string text = Console.ReadLine();
 int size = text.Length;
 int number = Convert.ToInt32(text); 
 int summ = 0;
-for (int count = 1; count <= size; count ++)
+for (int count = 0; count < size; count ++)
     {
     summ = summ + number % 10;
     number = number / 10;
